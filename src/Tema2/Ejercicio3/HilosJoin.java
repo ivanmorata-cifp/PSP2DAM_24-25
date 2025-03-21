20dd7b9a0f13985e922ef7f55c147db6f8aa6f8f
ivanmorata <ivan.morata@cifpceuta.es>
2025-03-21 17:54:48 +0100

    v5.1 -Añadidos comentarios explicativos en el ejercicio 3 del tema 2.

--- src/Tema2/Ejercicio3/HilosJoin.java
+++ src/Tema2/Ejercicio3/HilosJoin.java
@@ -1,15 +1,26 @@
 package Tema2.Ejercicio3;
 
+/*
+    Guía para resolver ejercicios de multi-hilo (NO-ProdCon)
+    1.Crear clase para representar el recurso compartido
+    2.Poner el acceso al recurso compartido (normalmente set/get) como synchronized
+    3.Crear clase con Runnable (simulador de hilos)
+        3.1. Como atributo, esta clase siempre tiene un objeto de la clase del recurso compartido
+        3.2. El constructor de la clase debe hacer copia superficial del atributo
+        3.3. Se plantea la ejecución del método run
+    4.(opcional) Generar en el main objetos de la clase Runnable con un objeto de la clase del recurso compartido
+ */
+
 public class HilosJoin implements Runnable {
 
     private TiradaDados dados;
 
     public HilosJoin (TiradaDados m) {
-        dados = m;
+        dados = m; //2. Cuando trabajamos con objetos compartidos, usamos copia superficial
     }
 
-    public void run ( ) {
-        try {
+    public void run ( ) { //3. El método run es obligatorio. (Implements) Runnable / (Extends) Thread
+        try { //4. Dentro del método run está la ejecución principal del programa
             Thread.sleep (1000);
             int resultadoDado = (int) (Math.random() * 6 ) + 1;
             dados.setSumaTirada ( resultadoDado );
@@ -19,24 +30,30 @@ public class HilosJoin implements Runnable {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        TiradaDados dados = new TiradaDados(0);
+    public static void main(String[] args) throws InterruptedException { //5. Para probar el programa concurrente:
+        TiradaDados dados = new TiradaDados(0); //5.1. Generar objeto de recurso compartido
 
         HilosJoin obj1 = new HilosJoin(dados);
         HilosJoin obj2 = new HilosJoin(dados);
         HilosJoin obj3 = new HilosJoin(dados);
+        //5.2. Generar objetos de la clase que implementa Runnable / hereda de Thread para cada hilo
+
         Thread hilo1 = new Thread(obj1);
         hilo1.setName("Dado 1");
         Thread hilo2 = new Thread(obj2);
         hilo2.setName("Dado 2");
         Thread hilo3 = new Thread(obj3);
         hilo3.setName("Dado 3");
+        //5.3. Inicializar los hilos con los objetos de la clase anterior
+
         hilo1.start();
         hilo2.start();
         hilo3.start();
+        //5.4. Usar el método start en todos los hilos
         hilo1.join();
         hilo2.join();
         hilo3.join();
+        //5.5. Llamar a join en todos los hilos para que el main espere la finalización de todos los hilos
 
         System.out.println("Total tirada: " + dados.getSumaTirada());
         System.out.println("Final Hilo Principal");
--- src/Tema2/Ejercicio3/TiradaDados.java
+++ src/Tema2/Ejercicio3/TiradaDados.java
@@ -1,17 +1,17 @@
 package Tema2.Ejercicio3;
 
-public class TiradaDados {
+public class TiradaDados { //1. Representar el objeto compartido
     private int tirada;
 
     public TiradaDados(int e) {
         tirada = e;
     }
 
-    public synchronized int getSumaTirada() {
+    public synchronized int getSumaTirada() { //1.1. Los métodos de acceso al recurso compartido deben ir synchronized
         return tirada;
     }
 
-    public synchronized void setSumaTirada (int e) {
+    public synchronized void setSumaTirada (int e) { //1.1 Igual que el anterior
         tirada += e;
     }
 }
